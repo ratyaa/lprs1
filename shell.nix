f025386cd9458d669a3150c62532b3861131e723
Ratibor Siryi <siryi.ra@phystech.edu>
2022-09-23 14:48:38 +0300

    Use Nix as package manager for Python.

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  python = pkgs.python39;
+  package = pkgs.python39Packages;
+  pythonPackages = python.buildEnv.override {
+    extraLibs = [
+      package.numpy
+      package.matplotlib
+      package.pandas
+      package.jupyter
+    ];
+  };
+  texPackages = pkgs.texlive.combined.scheme-full;
+in
+      
+pkgs.mkShell {
+  buildInputs = [
+    pythonPackages
+    texPackages
+  ];
+}
